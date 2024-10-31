7d3ce762d9b3bf21f5b43cf8c24a2c32215c7d0c
LordAves <alexander.may2910@gmail.com>
2024-10-31 18:48:37 +0100

    added centrality measures
    + betweenness centrality
    + closeness centrality

--- code/betweenness.R
+++ code/betweenness.R
@@ -16,9 +16,28 @@ nodes[nodes == "Environment_Condition"] <- NA
 edges <- read.csv("ferret_tables_Pruned_CCMN.csv_1 default edge.csv", header=TRUE)
 
 
+#set edgelist size from data
+edge_list <- matrix(nrow=length(edges[,1]),ncol=2)
+colnames(edge_list) <- c("From","To")
 
-bet_graph <- graph_from_data_frame(edges, directed = TRUE)
+#Get Interacting ASVs
+for (interaction in 1:(length(edges$name))){
+  #print(edges$name[interaction])
+  edge_list[interaction,1] <- substring(edges$name[interaction],1,12)
+  edge_list[interaction,2] <- substring(edges$name[interaction],31,42)
+}
 
-betweenness <- betweenness(bet_graph, directed = TRUE)
+#Make igraph object from edgelist
+edge_graph <- graph_from_edgelist(edge_list,directed=T)
 
+#calculate betweenness centrality
+betweenness <- betweenness(edge_graph, directed = T, normalized = T)
+
+
+#Overview
 summary(betweenness)
+
+betweenness[betweenness==min(betweenness)]
+betweenness[betweenness==max(betweenness)]
+
+sort(betweenness)
--- code/closeness.R
+++ code/closeness.R
@@ -16,9 +16,28 @@ nodes[nodes == "Environment_Condition"] <- NA
 edges <- read.csv("ferret_tables_Pruned_CCMN.csv_1 default edge.csv", header=TRUE)
 
 
+#set edgelist size from data
+edge_list <- matrix(nrow=length(edges[,1]),ncol=2)
+colnames(edge_list) <- c("From","To")
 
-close_graph <- graph_from_data_frame(edges, directed = TRUE)
+#Get Interacting ASVs
+for (interaction in 1:(length(edges$name))){
+  #print(edges$name[interaction])
+  edge_list[interaction,1] <- substring(edges$name[interaction],1,12)
+  edge_list[interaction,2] <- substring(edges$name[interaction],31,42)
+}
 
-closeness <- closeness(close_graph, vids=V(close_graph), mode= "all")
+#Make igraph object from edgelist
+edge_graph <- graph_from_edgelist(edge_list,directed=T)
 
+#calculate closeness centrality
+closeness <- closeness(edge_graph, mode= "all")
+
+
+#Overview
 summary(closeness)
+
+closeness[closeness==min(closeness)]
+closeness[closeness==max(closeness)]
+
+sort(closeness)
